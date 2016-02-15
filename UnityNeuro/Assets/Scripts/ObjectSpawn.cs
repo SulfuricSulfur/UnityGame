4a807f6dab349e2d261a242d34f69cc73c2f5799
Sulfur <rc7604@GDD-17.main.ad.rit.edu>
2016-02-15 17:42:01 -0500

    Attributes
    Added some attributes to the game script.

--- UnityNeuro/Assets/Scripts/ObjectSpawn.cs
+++ UnityNeuro/Assets/Scripts/ObjectSpawn.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 
 public class ObjectSpawn : MonoBehaviour {
+    public GameObject[] spawnedObjs;//the array of 6 objects that will spawn in
+    private GameObject target;//the object that has been chosen to be the target object the player will try and match
+    private int index;//the index of which the target is located in the spawnedObjs array
+
 
 	// Use this for initialization
 	void Start () {

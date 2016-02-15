10e728354b419fe7459a2e52cb979c6dbf3331be
Sulfur <rc7604@GDD-17.main.ad.rit.edu>
2016-02-15 18:20:31 -0500

    UI
    Set up the base scenes and UI for those scenes. The main menu will link
    to both the instructions and the trial select screen. The trial select
    screen will show buttons with the number of trials the user wishes to
    play on them. Each cycle is in all by itself 18sec long.  Clicking on a
    button from that screen will activate how many cycles the user wishes to
    play.

---
+++ UnityNeuro/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+    //the script controller for the main menu
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    //used for linking button to the instruction scene
+    public void GoToInstructions()
+    {
+
+    }
+
+    //used for linking start button to the select trial screen
+    public void StartGame()
+    {
+
+    }
+}
---
+++ UnityNeuro/Assets/Scripts/TrialSelect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrialSelect : MonoBehaviour {
+    //this script is for the user selecting however many trials they want to play
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
